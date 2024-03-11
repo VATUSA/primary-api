04d4fd72e0fc798247e2f749d159ab1837105e59
Raajheer1 <raaj.patel229@gmail.com>
2024-03-11 02:12:03 -0500

    lint fix

--- internal/v1/user-flag/user_flag.go
+++ internal/v1/user-flag/user_flag.go
@@ -15,7 +15,7 @@ type Request struct {
 	NoVisiting               bool `json:"no_visiting" example:"false"`
 	NoVisitingLogEntryID     uint `json:"no_visiting_log_entry_id" example:"1"`
 	NoTransferring           bool `json:"no_transferring" example:"false"`
-	NoTransferringLogEntryID uint `json:"no_transferring_log_entry_id" example:"`
+	NoTransferringLogEntryID uint `json:"no_transferring_log_entry_id" example:"1"`
 	NoTraining               bool `json:"no_training" example:"false"`
 	NoTrainingLogEntryID     uint `json:"no_training_log_entry_id" example:"1"`
 }
--- pkg/go-chi/setup.go
+++ pkg/go-chi/setup.go
@@ -2,12 +2,10 @@ package go_chi
 
 import (
 	"github.com/VATUSA/primary-api/pkg/config"
-	middleware2 "github.com/VATUSA/primary-api/pkg/middleware"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 	"github.com/go-chi/render"
-	"net/http"
 )
 
 func New(cfg *config.Config) *chi.Mux {
@@ -21,28 +19,5 @@ func New(cfg *config.Config) *chi.Mux {
 
 	r.Use(cors.Handler(NewCors(cfg)))
 
-	Testers(r)
-
 	return r
-}
-
-func Testers(r *chi.Mux) {
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Root."))
-	})
-
-	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("pong."))
-	})
-
-	r.Get("/panic", func(w http.ResponseWriter, r *http.Request) {
-		panic("test")
-	})
-
-	// Use NotGuest middleware on get route
-	r.Get("/guest", func(w http.ResponseWriter, r *http.Request) {
-		middleware2.NotGuest(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("Passed"))
-		})).ServeHTTP(w, r)
-	})
 }
