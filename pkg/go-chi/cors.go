1a999fbdf8784960ff9a83db5587e0e0a0c6eba0
Raajheer1 <raaj.patel229@gmail.com>
2024-06-30 07:53:45 +1000

    extend allowed origins

--- pkg/go-chi/cors.go
+++ pkg/go-chi/cors.go
@@ -3,11 +3,13 @@ package go_chi
 import (
 	"github.com/VATUSA/primary-api/pkg/config"
 	"github.com/go-chi/cors"
+	"strings"
 )
 
 func NewCors(cfg *config.Config) cors.Options {
+	allowedOrigins := strings.Split(cfg.Cors.AllowedOrigins, ",")
 	return cors.Options{
-		AllowedOrigins:   []string{cfg.Cors.AllowedOrigins},
+		AllowedOrigins:   allowedOrigins,
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "x-guest", "x-user", "x-api-key"},
 		AllowCredentials: true,
