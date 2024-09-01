da31edf03a1f23c031ccbdae00ada43846cfd8c9
Raajheer1 <raaj.patel229@gmail.com>
2024-09-01 12:24:22 -0500

    allow PATCH

--- pkg/go-chi/cors.go
+++ pkg/go-chi/cors.go
@@ -3,14 +3,16 @@ package go_chi
 import (
 	"github.com/VATUSA/primary-api/pkg/config"
 	"github.com/go-chi/cors"
+	log "github.com/sirupsen/logrus"
 	"strings"
 )
 
 func NewCors(cfg *config.Config) cors.Options {
 	allowedOrigins := strings.Split(cfg.Cors.AllowedOrigins, ",")
+	log.Debug("Allowed Origins: ", allowedOrigins)
 	return cors.Options{
 		AllowedOrigins:   allowedOrigins,
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedMethods:   []string{"GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "x-guest", "x-user", "x-api-key"},
 		AllowCredentials: true,
 		MaxAge:           300,
